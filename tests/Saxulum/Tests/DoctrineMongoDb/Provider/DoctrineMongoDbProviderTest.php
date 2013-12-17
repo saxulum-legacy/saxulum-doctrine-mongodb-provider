1589291160433b30852697904ef8ae69225556d2
Dominik Zogg <dominik.zogg@gmail.com>
2013-12-17 23:18:24 +0100

    add some initial test code

---
+++ tests/Saxulum/Tests/DoctrineMongoDb/Provider/DoctrineMongoDbProviderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Saxulum\Tests\DoctrineMongoDb\Provider;
+
+use Doctrine\MongoDB\Connection;
+use Saxulum\DoctrineMongoDb\Silex\Provider\DoctrineMongoDbProvider;
+use Silex\Application;
+
+class DoctrineMongoDbProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSingleConnection()
+    {
+        if(!extension_loaded('mongo')) {
+            $this->markTestSkipped('mongo is not available');
+        }
+
+        $app = new Application();
+        $app->register(new DoctrineMongoDbProvider(), array(
+            'mongodb.options' => array(
+                'username' => 'root',
+                'password' => 'root',
+                'db' => 'admin'
+            ),
+        ));
+
+        /** @var Connection $mongodb */
+        $mongodb = $app['mongodb'];
+
+        $this->assertSame($app['mongodbs']['default'], $mongodb);
+
+        try {
+            $database = $mongodb->selectDatabase('doctrine-mongodb-provider');
+            $collection = $database->selectCollection('sample');
+
+            $document = array('key' => 'value');
+            $collection->insert($document);
+
+            $this->assertArrayHasKey('_id', $document);
+
+            $database->dropCollection('sample');
+
+        } catch(\MongoConnectionException $e) {
+            $this->markTestSkipped('can\'t connect to the server');
+        }
+    }
+}
